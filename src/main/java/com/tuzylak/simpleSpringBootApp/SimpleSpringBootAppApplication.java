cdc9db4bf68c3d3df7d75ec6401a6bc42d25f038
Tuz <mstuzy@gmail.com>
2019-02-08 11:24:02 +0000

    added log4j as default logger

--- src/main/java/com/tuzylak/simpleSpringBootApp/SimpleSpringBootAppApplication.java
+++ src/main/java/com/tuzylak/simpleSpringBootApp/SimpleSpringBootAppApplication.java
@@ -1,8 +1,5 @@
 package com.tuzylak.simpleSpringBootApp;
 
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,17 +7,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class SimpleSpringBootAppApplication extends SpringBootServletInitializer
 {
-  
-  private static final Logger LOGGER = LogManager.getLogger(SimpleSpringBootAppApplication.class);
-  
-	public static void main(String[] args) {
-	  SpringApplication.run(SimpleSpringBootAppApplication.class, args);
-		
-    
-    LOGGER.info("Info level log message");
-    LOGGER.debug("Debug level log message");
-    LOGGER.error("Error level log message");
-}
+  public static void main(String[] args)
+  {
+    SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+
+  }
 
 }
-
