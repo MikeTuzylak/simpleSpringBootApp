6a3f6840427bd1ebeadf28fd76910946f4adb519
Tuz <mstuzy@gmail.com>
2019-02-08 10:19:59 +0000

    updated files for logging

--- src/main/java/com/tuzylak/simpleSpringBootApp/MyController.java
+++ src/main/java/com/tuzylak/simpleSpringBootApp/MyController.java
@@ -3,9 +3,12 @@ package com.tuzylak.simpleSpringBootApp;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Mike Tuzylak (07-02-2019)
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @Controller
 public class MyController
 {
+  private static final Logger LOGGER = LogManager.getLogger(SimpleSpringBootAppApplication.class);
+  
   
   Map<String, String> towns = new HashMap<>();
   
@@ -32,4 +37,17 @@ public class MyController
   {
     return "welcome";
   }
+  
+  @RequestMapping("/logger")
+  public String logger() {
+   
+    LOGGER.debug("This is a debug message");
+    LOGGER.info("This is an info message");
+    LOGGER.warn("This is a warn message");
+    LOGGER.error("This is an error message");
+   
+    return "welcome";
+  }
+  
+  
 }
--- src/main/java/com/tuzylak/simpleSpringBootApp/SimpleSpringBootAppApplication.java
+++ src/main/java/com/tuzylak/simpleSpringBootApp/SimpleSpringBootAppApplication.java
@@ -1,14 +1,26 @@
 package com.tuzylak.simpleSpringBootApp;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleSpringBootAppApplication {
-
+public class SimpleSpringBootAppApplication extends SpringBootServletInitializer
+{
+  
+  private static final Logger LOGGER = LogManager.getLogger(SimpleSpringBootAppApplication.class);
+  
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
-	}
+	  SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+		
+    
+    LOGGER.info("Info level log message");
+    LOGGER.debug("Debug level log message");
+    LOGGER.error("Error level log message");
+}
 
 }
 
